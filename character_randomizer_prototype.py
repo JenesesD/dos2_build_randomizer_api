a4d6667e6c78c1c9ee24d005b229b43f95a3c0ad
JenesesD <jenesesd@gmail.com>
2020-12-10 15:42:22 +0100

    defined pick_talent, comments

--- character_randomizer_prototype.py
+++ character_randomizer_prototype.py
@@ -16,15 +16,25 @@ GENDER = ["Male", "Female"]
 # Eternal is not present for a reason, there is no such thing when
 # you create a character
 RACE = ["Human", "Elf", "Dwarf", "Lizard"]
+
+# For the time being getting an undead character or not will be
+# decided like this
 UNDEAD = ["Yes", "No"]
+
+# For attributes and abilities duplicates is still possible
 ATTRIBUTES = ["strength", "Finesse", "Intelligence",
               "Constitution", "Memory", "Wits"]  # need 3
 ABILITIES = ["Dual Wielding", "Ranged", "Single-Handed", "Two-Handed",
              "Leadership", "Perserverence", "Retribution", "Aerotheurge", "Geomancer",
              "Huntsman", "Hydrosophist", "Necromancer", "Polymorph", "Pyrokinetic",
-             "Scoundrel", "Summoning"]
+             "Scoundrel", "Summoning"]  # need 3
 CIVIL_ABILITIES = ["Telkinesis", "Loremaster", "Sneaking", "Thievery", "Bartering",
                    "Persuasion", "Lucky Charm"]  # need 2
+
+# Talents are random, it is possible to get the same talents after multiple draws,
+# if and when the problem arises rolling another talent will be the solution,
+# another idea could be to roll all the players talent that they need to learn throughout the game
+# (would require extensive ruleset, maybe in the future).
 TALENTS = ["All Skilled Up", "Ambidextrous", "Arrow Recovery", "Bigger and better",
            "Comeback Kid", "Demon", "Duck Duck Goose", "Elemental Affinity", "Escapist", "Executioner",
            "Elemental Ranger", "Far Out Man", "Five-Star Diner", "Glass Cannon", "Guerrilla",
@@ -43,6 +53,10 @@ def character_generator():
     print(f"\nTalent: {choice(TALENTS)}")
 
 
+def pick_talent():
+    print(f"\nYour next talent: {choice(TALENTS)}")
+
+
 def main():
     print(f"(1) Generate Character")
     print(f"(2) Draw Talent")
@@ -51,7 +65,7 @@ def main():
     if user_input == "1":
         character_generator()
     elif user_input == "2":
-        pass
+        pick_talent()
     else:
         pass
 
