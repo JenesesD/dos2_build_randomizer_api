600cbc8e74159276ce024475fd987fdf35311d57
JenesesD <jenesesd@gmail.com>
2021-06-10 10:57:58 +0200

    hardcoded the items that need to be randomized, added TODO

--- src/main/java/randomizer/Main.java
+++ src/main/java/randomizer/Main.java
@@ -1,7 +1,26 @@
 package randomizer;
 
+import java.util.Arrays;
+import java.util.List;
+
 public class Main {
     public static void main(String[] args) {
+        // For the time being these Lists will be hardcoded here
+        // TODO: Create a database around it
+
+        // Need 3 Attributes
+        List<String> ATTRIBUTES = Arrays.asList("Strength", "Finesse", "Intelligence",
+                "Constitution", "Memory", "Wits");
+
+        // Need 2 Abilities
+        List<String> ABILITIES = Arrays.asList("Dual Wielding", "Ranged", "Single-Handed", "Two-Handed", "Leadership", "Perserverance",
+                "Retribution", "Aerotheurge", "Geomancer", "Huntsman", "Hydrosophist", "Necromancer", "Polymorph",
+                "Pyrokinetic", "Scoundrel", "Summoning", "Warfare");
+
+        //Need 2 Civil Abilities
+        List<String> CIVIL_ABILITIES = Arrays.asList("Telekinesis", "Loremaster", "Sneaking", "Thievery", "Bartering",
+                "Persuasion", "Lucky Charm");
+
 
     }
 }
