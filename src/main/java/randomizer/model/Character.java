97a0b6f0875fd94f3e18e224efd492cf43f7c932
JenesesD <jenesesd@gmail.com>
2021-06-10 10:54:29 +0200

    added lombok to pom.xml, annotations on Character class

--- src/main/java/randomizer/model/Character.java
+++ src/main/java/randomizer/model/Character.java
@@ -1,11 +1,19 @@
 package randomizer.model;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
 import java.util.List;
 
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class Character {
 
     private Gender gender;
     private Race race;
+    private boolean undead;
     private List<String> attributes;
     private List<String> abilities;
     private List<String> civilAbilities;
