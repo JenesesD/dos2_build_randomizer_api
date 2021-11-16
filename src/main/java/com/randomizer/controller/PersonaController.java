7ed4a4a46f5f9bfe9f47f8651e8600d3f72c31d4
JenesesD <jenesesd@gmail.com>
2021-11-16 14:44:23 +0100

    /character endpoint is live, arbitrary amount can be specified

--- src/main/java/com/randomizer/controller/PersonaController.java
+++ src/main/java/com/randomizer/controller/PersonaController.java
@@ -1,41 +1,29 @@
 package com.randomizer.controller;
 
 import com.randomizer.model.Persona;
+import com.randomizer.service.Randomizer;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.util.Arrays;
+import java.util.ArrayList;
 import java.util.List;
+import java.util.Random;
+import java.util.concurrent.atomic.AtomicLong;
 
 @RestController
 public class PersonaController {
-        // For the time being these Lists will be hardcoded here
-        // TODO: Create a database around it
-
-        // Need 3 Attributes
-        List<String> ATTRIBUTES = Arrays.asList("Strength", "Finesse", "Intelligence",
-                "Constitution", "Memory", "Wits");
-
-        // Need 2 Abilities
-        List<String> ABILITIES = Arrays.asList("Dual Wielding", "Ranged", "Single-Handed", "Two-Handed", "Leadership", "Perserverance",
-                "Retribution", "Aerotheurge", "Geomancer", "Huntsman", "Hydrosophist", "Necromancer", "Polymorph",
-                "Pyrokinetic", "Scoundrel", "Summoning", "Warfare");
-
-        //Need 2 Civil Abilities
-        List<String> CIVIL_ABILITIES = Arrays.asList("Telekinesis", "Loremaster", "Sneaking", "Thievery", "Bartering",
-                "Persuasion", "Lucky Charm");
-
-        List<String> TALENTS = Arrays.asList("All Skilled Up", "Ambidextrous", "Arrow Recovery", "Bigger and better",
-                "Comeback Kid", "Demon", "Duck Duck Goose", "Elemental Affinity", "Escapist", "Executioner",
-                "Elemental Ranger", "Far Out Man", "Five-Star Diner", "Glass Cannon", "Guerrilla",
-                "Hothead", "Ice King", "Leech", "Living Armour", "Lonewolf", "Mnemonic", "Morning Person",
-                "Opportunist", "Parry Master", "Pet Pal", "Picture of Health", "Savage Sortilege", "Slingshot",
-                "Stench", "The Pawn", "Torturer", "Unstable", "Walk It Off", "What a Rush");
-
+    Random random = new Random();
+    Randomizer randomizer = new Randomizer();
+    // private final AtomicLong counter = new AtomicLong();
 
     @GetMapping("/character")
-    public Integer getPersona(@RequestParam(value = "amount") String amount) {
-        return null;
+    public List<Persona> getPersona(@RequestParam(value = "amount", defaultValue = "1")Integer amount) {
+        List<Persona> characterList = new ArrayList<>();
+        for (int i = 0; i < amount; i++) {
+            String race = randomizer.getRace(random);
+            characterList.add(new Persona(race));
+        }
+        return characterList;
     }
 }
