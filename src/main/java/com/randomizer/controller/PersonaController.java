7210a649a1c0e3235b7ec902a571996677090cfc
JenesesD <jenesesd@gmail.com>
2021-11-17 15:41:49 +0100

    removed requestmapping annotation

--- src/main/java/com/randomizer/controller/PersonaController.java
+++ src/main/java/com/randomizer/controller/PersonaController.java
@@ -4,20 +4,18 @@ import com.randomizer.model.Persona;
 import com.randomizer.service.RandomizerService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 
 @RestController
-@RequestMapping("/character")
 public class PersonaController {
 
     @Autowired
     RandomizerService randomizerService;
 
-    @GetMapping("/")
+    @GetMapping("/character")
     public List<Persona> getPersona(@RequestParam(value = "amount", defaultValue = "1")Integer amount) {
         return randomizerService.getCharacters(amount);
     }
