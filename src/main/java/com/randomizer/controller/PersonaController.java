1df0158e39d5c7245aec6c99bb4f9e63722105e0
JenesesD <jenesesd@gmail.com>
2021-11-17 14:41:52 +0100

    modified controller, to only call the required method

--- src/main/java/com/randomizer/controller/PersonaController.java
+++ src/main/java/com/randomizer/controller/PersonaController.java
@@ -1,7 +1,7 @@
 package com.randomizer.controller;
 
 import com.randomizer.model.Persona;
-import com.randomizer.service.Randomizer;
+import com.randomizer.service.RandomizerService;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
@@ -10,10 +10,10 @@ import java.util.List;
 
 @RestController
 public class PersonaController {
-    Randomizer randomizer = new Randomizer();
+    RandomizerService randomizerService = new RandomizerService();
 
     @GetMapping("/character")
     public List<Persona> getPersona(@RequestParam(value = "amount", defaultValue = "1")Integer amount) {
-        return randomizer.getCharacters(amount);
+        return randomizerService.getCharacters(amount);
     }
 }
