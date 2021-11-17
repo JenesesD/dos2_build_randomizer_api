586bf0566caafa908159bdf036b276338c8370e6
JenesesD <jenesesd@gmail.com>
2021-11-17 15:46:26 +0100

    created getRace method

--- src/main/java/com/randomizer/repository/PersonaRepository.java
+++ src/main/java/com/randomizer/repository/PersonaRepository.java
@@ -2,6 +2,11 @@ package com.randomizer.repository;
 
 import com.randomizer.model.Persona;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 public interface PersonaRepository extends JpaRepository<Persona, Long> {
+
+    @Query(value = "SELECT name FROM races WHERE id = :id", nativeQuery = true)
+    String getRace(@Param("id") int id);
 }
