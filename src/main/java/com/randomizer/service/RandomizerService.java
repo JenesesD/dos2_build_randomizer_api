8c13f4354be0ad2259c1b21211795a5242c279f3
JenesesD <jenesesd@gmail.com>
2021-11-17 15:44:35 +0100

    autowired storage layer, modified class for a single public data collector method

--- src/main/java/com/randomizer/service/RandomizerService.java
+++ src/main/java/com/randomizer/service/RandomizerService.java
@@ -1,14 +1,29 @@
 package com.randomizer.service;
 
+import com.randomizer.dao.PersonaStorage;
 import com.randomizer.model.Persona;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Random;
 
+@Service
 public class RandomizerService {
 
+    @Autowired
+    PersonaStorage personaStorage;
+
     public List<Persona> getCharacters(int n) {
-        Random random = new Random();
-        return null;
+//        Random random = new Random();
+        List<Persona> personas = new ArrayList<>();
+        for (int i = 0; i < n; i++) {
+            String race = personaStorage.getRace(3);
+            personas.add(new Persona(i + 1, race));
+        }
+        return personas;
     }
+
+
 }
