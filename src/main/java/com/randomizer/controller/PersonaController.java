f3b9eb1a6f3b0f988761aeecdab99250060f2dca
JenesesD <jenesesd@gmail.com>
2021-11-17 15:40:04 +0100

    autowired randomizerService

--- src/main/java/com/randomizer/controller/PersonaController.java
+++ src/main/java/com/randomizer/controller/PersonaController.java
@@ -2,17 +2,22 @@ package com.randomizer.controller;
 
 import com.randomizer.model.Persona;
 import com.randomizer.service.RandomizerService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 
 @RestController
+@RequestMapping("/character")
 public class PersonaController {
-    RandomizerService randomizerService = new RandomizerService();
 
-    @GetMapping("/character")
+    @Autowired
+    RandomizerService randomizerService;
+
+    @GetMapping("/")
     public List<Persona> getPersona(@RequestParam(value = "amount", defaultValue = "1")Integer amount) {
         return randomizerService.getCharacters(amount);
     }
