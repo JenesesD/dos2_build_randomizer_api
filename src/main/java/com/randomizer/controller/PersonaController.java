a37e381490a6f92b92dd5c1b6f38f89bc2c36aa1
JenesesD <jenesesd@gmail.com>
2021-11-17 14:30:09 +0100

    modified controller class, removed redundant class

--- src/main/java/com/randomizer/controller/PersonaController.java
+++ src/main/java/com/randomizer/controller/PersonaController.java
@@ -6,24 +6,14 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.util.ArrayList;
 import java.util.List;
-import java.util.Random;
-import java.util.concurrent.atomic.AtomicLong;
 
 @RestController
 public class PersonaController {
-    Random random = new Random();
     Randomizer randomizer = new Randomizer();
-    // private final AtomicLong counter = new AtomicLong();
 
     @GetMapping("/character")
     public List<Persona> getPersona(@RequestParam(value = "amount", defaultValue = "1")Integer amount) {
-        List<Persona> characterList = new ArrayList<>();
-        for (int i = 0; i < amount; i++) {
-            String race = randomizer.getRace(random);
-            characterList.add(new Persona(race));
-        }
-        return characterList;
+        return randomizer.getCharacters(amount);
     }
 }
