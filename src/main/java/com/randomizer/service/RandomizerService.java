9ad4a42ae91d111fda1528bb60a94bc6c8a611d6
JenesesD <jenesesd@gmail.com>
2021-11-17 14:39:34 +0100

    modified corresponding service class

---
+++ src/main/java/com/randomizer/service/RandomizerService.java
@@ -0,0 +1,14 @@
+package com.randomizer.service;
+
+import com.randomizer.model.Persona;
+
+import java.util.List;
+import java.util.Random;
+
+public class RandomizerService {
+
+    public List<Persona> getCharacters(int n) {
+        Random random = new Random();
+        return null;
+    }
+}
