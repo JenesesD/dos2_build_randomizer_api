6ff290b26bd0493063971c2f2cbe4a0c41c54fe1
JenesesD <jenesesd@gmail.com>
2021-11-18 16:08:42 +0100

    minor adjustments in field order, modified constructor

--- src/main/java/com/randomizer/model/Persona.java
+++ src/main/java/com/randomizer/model/Persona.java
@@ -9,18 +9,17 @@ public class Persona {
     @Id
     private final Integer id;
     private final String race;
-    private final boolean undead;
     private final String gender;
+    private final boolean undead;
     private final String attributes;
     private final String abilities;
     private final String civilAbility;
-//    private final String skills;
+    private final String skills;
     private final String talent;
     private final String instrument;
 
-    // String skills, String talent, String instrument
     public Persona(Integer id, String race, String gender, boolean undead, String attributes, String abilities,
-                   String civilAbility, String talent, String instrument) {
+                   String civilAbility, String skills, String talent, String instrument) {
         this.id = id;
         this.race = race;
         this.gender = gender;
@@ -28,7 +27,7 @@ public class Persona {
         this.attributes = attributes;
         this.abilities = abilities;
         this.civilAbility = civilAbility;
-//        this.skills = skills;
+        this.skills = skills;
         this.talent = talent;
         this.instrument = instrument;
     }
@@ -60,10 +59,10 @@ public class Persona {
     public String getCivilAbility() {
         return civilAbility;
     }
-//    public String getSkills() {
-//        return skills;
 
-//    }
+    public String getSkills() {
+        return skills;
+    }
 
     public String getTalent() {
         return talent;
