290aaccec3344d35f1071a092f33baabb2aea0f2
JenesesD <jenesesd@gmail.com>
2021-11-18 14:05:52 +0100

    created query to get all attributes from the db

--- src/main/java/com/randomizer/repository/PersonaRepository.java
+++ src/main/java/com/randomizer/repository/PersonaRepository.java
@@ -5,8 +5,13 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 
+import java.util.List;
+
 public interface PersonaRepository extends JpaRepository<Persona, Long> {
 
     @Query(value = "SELECT name FROM races WHERE id = :id", nativeQuery = true)
     String getRace(@Param("id") int id);
+
+    @Query(value = "SELECT name FROM attributes", nativeQuery = true)
+    List<String> getAttributes();
 }
