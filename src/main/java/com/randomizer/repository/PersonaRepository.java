562ad1e9578f1d1206ac71273a1383d1a1a271cd
JenesesD <jenesesd@gmail.com>
2021-11-18 14:25:22 +0100

    created query that gets all abilites from the db, added comments

--- src/main/java/com/randomizer/repository/PersonaRepository.java
+++ src/main/java/com/randomizer/repository/PersonaRepository.java
@@ -9,9 +9,16 @@ import java.util.List;
 
 public interface PersonaRepository extends JpaRepository<Persona, Long> {
 
+    // Query that selects a single race based on the parameter id
     @Query(value = "SELECT name FROM races WHERE id = :id", nativeQuery = true)
     String getRace(@Param("id") int id);
 
+    // Not sure which would be more efficient, one "large" query or two "small" queries
+    // Query that selects all attributes
     @Query(value = "SELECT name FROM attributes", nativeQuery = true)
     List<String> getAttributes();
+
+    // Query that selects all abilities
+    @Query(value = "SELECT name FROM abilities", nativeQuery = true)
+    List<String> getAbilities();
 }
