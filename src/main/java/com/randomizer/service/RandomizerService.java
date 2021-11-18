a62bc6d964b4abe1802cee3ceb53a1abb9fdf401
JenesesD <jenesesd@gmail.com>
2021-11-18 14:07:58 +0100

    method that generates two random non-matching elements, helper method

--- src/main/java/com/randomizer/service/RandomizerService.java
+++ src/main/java/com/randomizer/service/RandomizerService.java
@@ -6,6 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.ArrayList;
+import java.util.LinkedList;
 import java.util.List;
 import java.util.Random;
 
@@ -16,22 +17,46 @@ public class RandomizerService {
     PersonaStorage personaStorage;
 
     public List<Persona> getCharacters(int n) {
+
         // Create an instance of Random, so it can be passed to auxiliary methods
         Random random = new Random();
 
         // List that will contain the generated characters
         List<Persona> personas = new ArrayList<>();
-        
+
         for (int i = 0; i < n; i++) {
             String race = personaStorage.getRace(generateRandomNumber(random, 4));
             String gender = generateRandomNumber(random, 100) > 50 ? "Male" : "Female";
-            personas.add(new Persona(i + 1, race, gender));
+            List<String> attributes = personaStorage.getAttributes();
+            String selectedAttributes = joinListsIntoString(randomizeAttributes(random, attributes));
+            personas.add(new Persona(i + 1, race, gender, selectedAttributes));
         }
         return personas;
     }
 
     // Method to help comprehension and readability
+    // Generates a random number between 1 and n (inclusive)
     private int generateRandomNumber(Random random, int n) {
         return 1 + random.nextInt((n - 1) + 1);
+    }
+
+    // Method to help comprehension and readability
+    // Joins list items together
+    private String joinListsIntoString(List<String> list) { return String.join(", ", list); }
+
+    // Method that will randomly select two non-matching attributes
+    private List<String> randomizeAttributes(Random random, List<String> attributes) {
+        List<String> randomAttributes = new LinkedList<>();
+
+        // Necessary while loop in order to select non-matching values
+        while (randomAttributes.size() != 2) {
+            int randomNumber = random.nextInt(attributes.size());
+
+            // Checks if the attribute selected is already in the randomAttributes List
+            if (!randomAttributes.contains(attributes.get(randomNumber))) {
+                randomAttributes.add(attributes.get(randomNumber));
+            }
+        }
+        return randomAttributes;
     }
 }
