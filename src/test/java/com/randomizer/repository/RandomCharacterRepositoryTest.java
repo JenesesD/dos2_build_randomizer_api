6e4ce578448ce14e8ff17b473f63f3ab58f66b19
JenesesD <jenesesd@gmail.com>
2021-12-02 15:51:20 +0100

    created test cases for getRace method

--- src/test/java/com/randomizer/repository/RandomCharacterRepositoryTest.java
+++ src/test/java/com/randomizer/repository/RandomCharacterRepositoryTest.java
@@ -1,13 +1,44 @@
 package com.randomizer.repository;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 
+import java.util.Arrays;
+import java.util.List;
+
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.instanceOf;
+import static org.junit.jupiter.api.Assertions.*;
+
+@DataJpaTest(showSql = false)
+@AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.NONE)
 class RandomCharacterRepositoryTest {
 
+    @Autowired
+    private RandomCharacterRepository repository;
+
     @Test
-    void getRaceById() {
+    void getRaceByIdNotNull() {
+        assertNotNull(repository.getRace(3));
     }
 
+    @Test
+    void getRaceByIdReturnStringValue() { assertThat(repository.getRace(3), instanceOf(String.class)); }
+
+    @Test
+    void getRaceByIdReturnAppropriateValue() {
+        List<String> races = Arrays.asList("Dwarf", "Elf", "Lizard", "Human");
+        String race = repository.getRace(3);
+        assertTrue(races.contains(race));
+    }
+
+    @Test
+    void getRaceByIdReturnNullWithInvalidId() {
+        assertNull(repository.getRace(8));
+    }
+    
     @Test
     void getAttributes() {
     }
